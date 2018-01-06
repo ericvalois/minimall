5f2415431759817f664ed64cbb5539235692650b
Eric Valois <eric.v@bulledev.com>
2018-01-06 12:53:27 -0500

    Fix wide gutenberg

--- includes/compatibility/gutenberg/gutenberg.php
+++ includes/compatibility/gutenberg/gutenberg.php
@@ -6,6 +6,7 @@ add_action( 'after_setup_theme', 'minimall_gutenberg_init' );
 function minimall_gutenberg_init(){
     add_theme_support( 'gutenberg', array(
         'wide-images' => true,
+        'align-wide' => true,
         'colors' => array(
             get_theme_mod('primary_color','#1078ff'),
             '#333',
