37a6a5b75da2abcee08003993284e7ebd8189866
Eric Valois <eric.v@bulledev.com>
2018-01-15 14:12:39 -0500

    fix EDD editor width

--- includes/admin/editor.php
+++ includes/admin/editor.php
@@ -27,7 +27,7 @@ function minimall_add_editor_styles() {
 
     add_editor_style( trailingslashit( get_template_directory_uri() ) . 'assets/css/editor.min.css' );
 
-    if( get_page_template_slug( $post->ID ) != '' || is_singular('download') ){
+    if( get_page_template_slug( $post->ID ) != '' || get_post_type( $post->ID ) == 'download' ){
         add_editor_style( trailingslashit( get_template_directory_uri() ) . 'assets/css/editor-full-width.min.css' );
     }
 }
