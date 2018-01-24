ecce552cac3d2a0dc83ca35799a7a637188021f7
Eric Valois <eric.v@bulledev.com>
2018-01-24 13:35:02 -0500

    Better EDD download template

--- template-parts/content-download.php
+++ template-parts/content-download.php
@@ -15,20 +15,28 @@
             <?php 
                 $firstColumn = get_theme_mod('edd_single_layout','6');
                 $secondColumn = 12 - $firstColumn;
+
+                if( $firstColumn == 12 ){
+                    $first_col_padding = '';
+                    $second_col_padding = '';
+                }else{
+                    $first_col_padding = 'lg-pr2';
+                    $second_col_padding = 'lg-pl2';
+                }
             ?>
-            <div class="lg-col-<?php echo esc_attr( $firstColumn ); ?> lg-pr2">
+            <div class="lg-col-<?php echo esc_attr( $firstColumn ); ?> <?php echo esc_attr( $first_col_padding ); ?>">
 
                 <?php do_action('minimall_edd_first_column'); ?>
 
             </div>
 
-            <div class="lg-col-<?php echo esc_attr( $secondColumn ); ?> lg-px2">
+            <div class="lg-col-<?php echo esc_attr( $secondColumn ); ?> <?php echo esc_attr( $second_col_padding ); ?>">
                 <?php do_action('minimall_edd_second_column'); ?>
             </div>
         </div>
 
         <?php if ( is_active_sidebar( 'download-tabs-sidebar' ) ): ?>
-            <section class="download-tabs-section mt3">
+            <section class="download-tabs-section mt2">
                 <ul class="minimall-tabs" role="tablist">
                     <?php do_action('minimall_edd_tabs'); ?>
                 </ul>
