5f46f712fc31bfe7893ba83ba91a186c202fc63a
Eric Valois <eric.v@bulledev.com>
2018-01-29 09:29:50 -0500

    Avancement better Gutenberg Compatibility

--- includes/compatibility/gutenberg/gutenberg.php
+++ includes/compatibility/gutenberg/gutenberg.php
@@ -13,3 +13,20 @@ function minimall_gutenberg_init(){
     );
     add_theme_support( 'align-wide' );
 }
+
+/*
+* Gutenberg Editor Styles
+*/
+add_action( 'enqueue_block_editor_assets', 'minimall_gutenberg_editor_styles' );
+function minimall_gutenberg_editor_styles() {
+    wp_enqueue_style( 'minimall-blocks-style', get_template_directory_uri() . '/includes/compatibility/gutenberg/gutenberg-editor.css');
+}
+
+
+/**
+ * Enqueue scripts and styles.
+ */
+add_action( 'wp_enqueue_scripts', 'minimall_gutenberg_styles' );
+function minimall_gutenberg_styles() {
+    wp_enqueue_style( 'minimall-gutenberg-style', trailingslashit( get_template_directory_uri() ) . '/includes/compatibility/gutenberg/gutenberg.css' );
+}
