b75439cac69e7a59d9b28dafd167e8fec204c279
Eric Valois <eric.v@bulledev.com>
2018-02-14 09:12:24 -0500

    Fix missing padding on mobile post content

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,7 +8,7 @@
  */
 
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
+<article id="post-<?php the_ID(); ?>" <?php post_class('px2'); ?> >
 	
     
     <?php if( is_single() ): ?>
